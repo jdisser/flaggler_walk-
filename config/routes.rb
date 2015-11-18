bb596cf5256a6667a9d52f1a442a33ff2bbaddd3
Amanda Ring-Rissler <amanda.ringrissler@gmail.com>
2015-11-18 18:57:06 -0500

    Added login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root to: 'sessions#new'
 
   resources :photos
   resources :itineraries
   resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
