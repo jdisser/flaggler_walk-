a4e6db724d751df3bbf67a0c00f43725047d0979
Amanda Ring-Rissler <amanda.ringrissler@gmail.com>
2015-11-18 19:09:02 -0500

    Wrote session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(username: params[:username]).
+      try(:authenticate, params[:password])
+
+      if @user
+        session[:user_id] = @user.id
+        redirect_to photos_path
+        #this will change later it will indicate the
+        #starting point for villagers after login  
+      else
+        render action: 'new'
+      end
+   end
 end
