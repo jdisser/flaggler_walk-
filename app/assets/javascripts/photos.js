9bae0e8f01489c303444ecea2494da39ba2f03df
Dominique Miller <dominique.p.miller@gmail.com>
2015-11-27 20:52:48 -0500

    changed check to radio button

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,1 +1,13 @@
+// site wide jquey effects
 
+$(document).on('page:change', function() {
+  $('#itinerary_travel_classcheck-box').on('click', function(){
+    $('.travel-mode').effect("shake");
+  });
+  $('#itinerary_travel_classcheck-box2').on('click', function(){
+    $('.travel-mode2').effect("shake");
+  });
+  $('#itinerary_travel_classcheck-box3').on('click', function(){
+    $('.travel-mode3').effect("shake");
+  });
+});
