5263d4765865f7505d2f756373a6531d1610cb5a
Dominique Miller <dominique.p.miller@gmail.com>
2015-11-28 19:59:05 -0500

    fixed lack of comma

--- app/assets/javascripts/itineraries.js
+++ app/assets/javascripts/itineraries.js
@@ -69,7 +69,7 @@ function setMarkers(locations, picData) {
           var markerArray = [];
           var markerImage = {                       //define the icon and anchor point
             // url: '/assets/picpinmarker.png',
-            url: 'https://s3.amazonaws.com/photastic/uploads/photo/upload/picpin/picpinmarker.png'
+            url: 'https://s3.amazonaws.com/photastic/uploads/photo/upload/picpin/picpinmarker.png',
             size: new google.maps.Size(25,25),
             origin: new google.maps.Point(0,0),
             anchor: new google.maps.Point(12,12)    //set to center of image
