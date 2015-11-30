ae3ef64e7eb71a47efc0fbc6cbfdb662867e4572
Dominique Miller <dominique.p.miller@gmail.com>
2015-11-30 06:59:38 -0500

    added popup for image thumnails

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -10,7 +10,8 @@ $(document).on('page:change', function() {
   $('#itinerary_travel_driving').on('click', function(){
     $('.travel-mode3').effect("shake");
   });
-  $('.thumnails').click(function(){
-      $('.thumbnails').bPopup();
-  });
+  // $('#test74').on('click', function(){
+  //   event.preventDefault();
+  //   $('#full74').bPopup();
+  // });
 });
